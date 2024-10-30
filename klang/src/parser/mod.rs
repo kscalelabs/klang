41c2621dee4cd4c17bb53e13e5a9e66d09e70a84
Benjamin Bolte <ben@bolte.cc>
2024-10-30 15:22:30 -0700

    refactor compiler to separate file

--- klang/src/parser/mod.rs
+++ klang/src/parser/mod.rs
@@ -16,9 +16,9 @@ use functions::parse_function_def;
 use pest::Parser;
 use std::fs;
 use std::path::Path;
-use structs::{PestParser, Rule};
+use structs::{KlangProgram, PestParser, Rule};
 
-pub fn parse_program(pair: pest::iterators::Pair<Rule>) -> Result<Program, ParseError> {
+pub fn parse_program(pair: pest::iterators::Pair<Rule>) -> Result<KlangProgram, ParseError> {
     let mut functions = Vec::new();
 
     for function_pair in pair.into_inner() {
@@ -37,17 +37,19 @@ pub fn parse_program(pair: pest::iterators::Pair<Rule>) -> Result<Program, Parse
         }
     }
 
-    Ok(Program { functions })
+    Ok(KlangProgram {
+        program: Program { functions },
+    })
 }
 
-pub fn parse_string(input: &str) -> Result<Program, ParseError> {
+pub fn parse_string(input: &str) -> Result<KlangProgram, ParseError> {
     match PestParser::parse(Rule::program, input) {
         Ok(mut pairs) => parse_program(pairs.next().unwrap()),
         Err(e) => Err(ParseError::new(format!("Error parsing input: {}", e))),
     }
 }
 
-pub fn parse_file(file_path: &Path) -> Result<Program, ParseError> {
+pub fn parse_file(file_path: &Path) -> Result<KlangProgram, ParseError> {
     let unparsed_file = fs::read_to_string(file_path).map_err(|e| {
         ParseError::new(format!(
             "Error reading file '{}': {}",
@@ -59,9 +61,9 @@ pub fn parse_file(file_path: &Path) -> Result<Program, ParseError> {
     parse_string(&unparsed_file)
 }
 
-pub fn write_program_to_file(program: &Program, file_path: &Path) -> Result<(), ParseError> {
+pub fn write_program_to_file(program: &KlangProgram, file_path: &Path) -> Result<(), ParseError> {
     let mut buf = Vec::new();
-    prost::Message::encode(program, &mut buf).map_err(|e| {
+    prost::Message::encode(&program.program, &mut buf).map_err(|e| {
         ParseError::new(format!(
             "Error encoding program to file '{}': {}",
             file_path.display(),
@@ -78,4 +80,17 @@ pub fn write_program_to_file(program: &Program, file_path: &Path) -> Result<(), 
     })?;
 
     Ok(())
+}
+
+pub fn read_program_from_file(file_path: &Path) -> Result<KlangProgram, ParseError> {
+    let buf = fs::read(file_path).map_err(|e| {
+        ParseError::new(format!(
+            "Error reading file '{}': {}",
+            file_path.display(),
+            e
+        ))
+    })?;
+    let program = prost::Message::decode(&*buf)
+        .map_err(|e| ParseError::new(format!("Error decoding program: {}", e)))?;
+    Ok(KlangProgram { program })
 }
--- klang/src/parser/structs.rs
+++ klang/src/parser/structs.rs
@@ -1,5 +1,10 @@
+use super::ast::Program;
 use pest_derive::Parser;
 
 #[derive(Parser)]
 #[grammar = "pest/klang.pest"]
 pub struct PestParser;
+
+pub struct KlangProgram {
+    pub program: Program,
+}
